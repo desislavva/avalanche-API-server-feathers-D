ed7fd7ae87eafe88e77388b876aaddcfd42643c0
Desi <dessislv28th@gmail.com>
2021-10-25 12:11:49 +0300

    init commit + add network activity functionality

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+const logger = require('./logger');
+const app = require('./app');
+const axios = require('axios');
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.SERVER_PORT;
+
+const server = app.listen(port);
+
+process.on('unhandledRejection', (reason, p) =>
+  logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
+
+server.on('listening', () =>
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
